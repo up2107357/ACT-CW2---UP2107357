b74bde25e7dfe5123b85b6580c5bf835cf3ad34e
Dylan Thompson <up2107357@myport.ac.uk>
2025-11-28 13:52:16 +0000

    marking the commands for the codespace terminal

--- py/functions.py
+++ py/functions.py
@@ -1,5 +1,4 @@
 print("cd first")
-pip install kaggle
-attrib -r C:\Users\dylan\.kaggle\kaggle.json
-kaggle datasets download -d UP2107357/amazon_books_metadata_sample_20k
-tar -xf amazon_books_metadata_sample_20k.zip
+#pip install kaggle
+#kaggle datasets download -d UP2107357/amazon_books_metadata_sample_20k
+#tar -xf amazon_books_metadata_sample_20k.zip
