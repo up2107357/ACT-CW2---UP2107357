ba9a5d2425e55bc83d27d64c4f1a114a8c956d61
Dylan Thompson <up2107357@myport.ac.uk>
2025-11-28 13:11:01 +0000

    Add commands for Kaggle dataset download

--- py/functions.py
+++ py/functions.py
@@ -1,1 +1,5 @@
 print("cd first")
+pip install kaggle
+attrib -r C:\Users\dylan\.kaggle\kaggle.json
+kaggle datasets download -d UP2107357/amazon_books_metadata_sample_20k
+tar -xf amazon_books_metadata_sample_20k.zip
